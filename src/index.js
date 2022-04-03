62b287b11f4ea8f13926459b45fa7af817265aa3
Romil Jain <61315825+jainromil56@users.noreply.github.com>
2022-04-03 12:17:23 +0530

    Updated link

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import '../src/Index.css';
+import '../src/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
